d18d6c91fc83c17aebac32df0ecf1bcc6ed76ebd
HelixA350 <168017385+HelixA350@users.noreply.github.com>
2024-08-09 10:02:52 +0300

    Update xl_work_class.py

--- Excel/xl_work_class.py
+++ Excel/xl_work_class.py
@@ -4,15 +4,15 @@ from xls2xlsx import XLS2XLSX
 
 class Xl_work:
     def __init__(self, web_src: str, bit_src: str) -> None:
-        self.paths = [web_src, bit_src]
+        self.Paths = [web_src, bit_src]
         self.pathDone = './hh.xlsx'
     
     def __make_link_files(self) -> dict:
         try:
-            x2x = XLS2XLSX(self.paths[1])
+            x2x = XLS2XLSX(self.Paths[1])
             wb = x2x.to_xlsx()
         except:
-            wb = oxl.load_workbook(filename=self.paths[1])
+            wb = oxl.load_workbook(filename=self.Paths[1])
         
         names = {}
         ws = wb.active
@@ -25,15 +25,15 @@ class Xl_work:
 
     def __create_sheets(self) -> None:
         try:
-            x2x = XLS2XLSX(self.paths[1])
+            x2x = XLS2XLSX(self.Paths[1])
             wb_bit = x2x.to_xlsx()
         except:
-            wb_bit = oxl.load_workbook(filename=self.paths[1])
+            wb_bit = oxl.load_workbook(filename=self.Paths[1])
         try:
-            x2x = XLS2XLSX(self.paths[0])
+            x2x = XLS2XLSX(self.Paths[0])
             wb_web = x2x.to_xlsx()
         except:
-            wb_web = oxl.load_workbook(filename=self.paths[0])
+            wb_web = oxl.load_workbook(filename=self.Paths[0])
         ws_web = wb_web.active
         ws_bit = wb_bit.active
         for i in range(1, ws_bit.max_row):
@@ -52,10 +52,10 @@ class Xl_work:
     
     def __spread_on_sheets(self, links: dict) -> None:
         try:
-            x2x = XLS2XLSX(self.paths[0])
+            x2x = XLS2XLSX(self.Paths[0])
             wb_web = x2x.to_xlsx()
         except:
-            wb_web = oxl.load_workbook(filename=self.paths[0])
+            wb_web = oxl.load_workbook(filename=self.Paths[0])
         ws_web = wb_web.active
         first = True
         for i, el in enumerate(ws_web["F"]):
