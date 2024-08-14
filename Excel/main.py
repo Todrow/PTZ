7741a1a94bdb3bbbd08a175baf13ff5339598b6a
HelixA350 <168017385+HelixA350@users.noreply.github.com>
2024-08-14 14:40:05 +0300

    Update main.py

--- Excel/main.py
+++ Excel/main.py
@@ -1,14 +1,14 @@
 from xl_work_class import Xl_work
 from exlWrapper import ExcelWrapper
 
-xl = Xl_work('C:/Users/pinchukna/Documents/GitHub/PTZ/Excel/w.xls', 'C:/Users/pinchukna/Documents/GitHub/PTZ/Excel/b.xlsx')
+xl = Xl_work(r'C:\Users\Aleksandr\Documents\Работа\Excel\Отчет по ПЭ от 02.08.2024.xls', r'C:\Users\Aleksandr\Documents\Работа\Excel\Выгрузка задачи Битрикс.xlsx')
 ew = ExcelWrapper()
 
 xl.start()
-pathDone = 'C:/Users/pinchukna/Documents/GitHub/PTZ/Excel/done.xlsx'
+pathDone = r'C:\Users\Aleksandr\Documents\Работа\Excel\Report.xlsx'
 wb = xl.open_file(pathDone)
 
-for sheet in wb.sheetnames[1:]:
+for sheet in wb.sheetnames[1:-1]:
     ew.formatTitles(wb[sheet], True)
     ew.formattingCells(wb[sheet])
 wb.save(pathDone)
