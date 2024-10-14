475038844850751fbda1bc605683e5d1b51e4303
HelixA350 <aleksandrfaleev3@gmail.com>
2024-10-14 12:09:42 +0300

    copy_btn

--- Excel/doc/static/doc.js
+++ Excel/doc/static/doc.js
@@ -1,3 +1,24 @@
-function copyToClipboard() {
+function copyToClipboard(button) {
+    // Находит <pre> в том же <div>, что и кнопка
+    var preElement = button.parentNode.querySelector('pre');
     
+    // Создает временное текстовое поле
+    var textarea = document.createElement('textarea');
+    textarea.value = preElement.textContent;
+    
+    // Добавляет это текстовое поле в <body>
+    document.body.appendChild(textarea);
+    
+    // Выделяет текст
+    textarea.select();
+
+    // Копирование через execCommand
+    try {
+        document.execCommand('copy');
+    } catch (err) {
+        alert('Не удалось скоприровать элемент');
+    }
+
+    // Удаляет созданное на время текстовое поле
+    document.body.removeChild(textarea);
 }
